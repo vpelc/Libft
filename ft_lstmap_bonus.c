deda0c2645cbf3529bda805c3b1a31ff0ca16700
Victor Pelc <vpelc@shi-r1-p4.s19.be>
2024-04-19 16:28:09 +0200

    Correction

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vpelc <vpelc@student.s19.be>               +#+  +:+       +#+        */
+/*   By: vpelc <vpelc@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 16:36:00 by vpelc             #+#    #+#             */
-/*   Updated: 2024/04/16 18:04:27 by vpelc            ###   ########.fr       */
+/*   Updated: 2024/04/19 15:45:32 by vpelc            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,16 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*dup;
 	t_list	*elem;
+	void	*set;
 
 	dup = 0;
 	while (lst)
 	{
-		elem = ft_lstnew(f(lst->content));
+		set = f(lst->content);
+		elem = ft_lstnew(set);
 		if (!elem)
 		{
-			del(lst->content);
+			del(set);
 			ft_lstclear(&dup, (*del));
 			return (dup);
 		}
--- ft_split.c
+++ ft_split.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vpelc <vpelc@student.s19.be>               +#+  +:+       +#+        */
+/*   By: vpelc <vpelc@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 15:13:54 by vpelc             #+#    #+#             */
-/*   Updated: 2024/04/16 18:43:50 by vpelc            ###   ########.fr       */
+/*   Updated: 2024/04/19 16:18:00 by vpelc            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ static	void	ft_putstr(char *split, char const *str, char c)
 	split[i] = '\0';
 }
 
-void	ft_put_in_split(char **split, char const *str, char c)
+static	int	ft_put_in_split(char **split, char const *str, char c)
 {
 	size_t	i;
 	size_t	j;
@@ -59,23 +59,37 @@ void	ft_put_in_split(char **split, char const *str, char c)
 			while (str[i + j] != c && str[i + j])
 				j++;
 			split[k] = malloc(sizeof(char) * j + 1);
+			if (!split[k])
+				return (0);
 			ft_putstr(split[k], str + i, c);
 			i += j;
 			k++;
 		}
 	}
+	return (1);
 }
 
 char	**ft_split(char const *str, char c)
 {
 	char	**split;
 	size_t	size;
+	int		k;
 
 	size = ft_nbrstr(str, c);
 	split = malloc((size + 1) * sizeof(char *));
 	if (!split)
 		return (0);
-	ft_put_in_split(split, str, c);
+	k = ft_put_in_split(split, str, c);
+	if (k == 0)
+	{
+		while (split[k])
+		{
+			free(split[k]);
+			k++;
+		}
+		free(split);
+		return (0);
+	}
 	split[size] = 0;
 	return (split);
 }
